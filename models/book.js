d1add00339a7305a15c9cf15c48f69b8b3c726a7
Evan Durkin <edurkin@my365.bellevue.edu>
2022-01-09 19:37:28 -0500

    updates

--- models/book.js
+++ models/book.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose")
-// const author = require("./author")
-const path = require("path")
 
-const coverImageBasePath = "uploads/bookCovers"
+
 
 const bookSchema = new mongoose.Schema({
     title: {
@@ -25,7 +23,11 @@ const bookSchema = new mongoose.Schema({
         required: true,
         default: Date.now
     },
-    coverImageName: {
+    coverImage: {
+        type: Buffer,
+        required: true
+    },
+    coverImageType: {
         type: String,
         required: true
     },
@@ -36,17 +38,11 @@ const bookSchema = new mongoose.Schema({
     }
 })
 
-bookSchema.virtual("coverImagePath").get(function() {
-    if (this.coverImageName != null) {
-        return path.join("/", coverImageBasePath, this.coverImageName)
-    }
-})
 
-bookSchema.virtual("coverImagePath").get(function() {
-    if (this.coverImageName != null) {
-        return path.join("/", coverImageBasePath, this.coverImageName)
+bookSchema.virtual('coverImagePath').get(function() {
+    if (this.coverImage != null && this.coverImageType != null) {
+      return `data:${this.coverImageType};charset=utf-8;base64,${this.coverImage.toString('base64')}`
     }
-})
+  })
 
 module.exports = mongoose.model("Book", bookSchema)
-module.exports.coverImageBasePath = coverImageBasePath
